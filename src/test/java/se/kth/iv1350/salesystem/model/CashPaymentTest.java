36634ef08614274fa624480415ecb6b5688e501a
klatist <klara@tistrand.com>
2025-04-28 13:22:35 +0200

    latest version

--- src/test/java/se/kth/iv1350/salesystem/model/CashPaymentTest.java
+++ src/test/java/se/kth/iv1350/salesystem/model/CashPaymentTest.java
@@ -1,8 +1,16 @@
 package se.kth.iv1350.salesystem.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CashPaymentTest {
+
+    
+    @BeforeEach
+    public void setup(){
+
+    }
+
     @Test
     void testGetChange() {
 
