78a2c9a545c7baaeeb8d33c5d0feb761bd828cdc
klatist <klara@tistrand.com>
2025-04-29 12:46:45 +0200

    add cart

--- src/main/java/se/kth/iv1350/salesystem/model/Cart.java
+++ src/main/java/se/kth/iv1350/salesystem/model/Cart.java
@@ -7,4 +7,52 @@ public class Cart {
     private List<Item> cart;
 
 
- 
+    public Cart(){
+        this.cart = new ArrayList<Item>();
+    }
+
+    public void add(Item item){
+        this.cart.add(item);
+
+    }
+
+    public Item getItem(int position){
+        return this.cart.get(position);
+    
+    }
+
+    public List<Item> getCart(){
+        return this.cart;
+    }
+
+    public List<ItemDTO> toItemDTOCart(){
+        List<ItemDTO> itemDTOs = new ArrayList<ItemDTO>();
+        for (Item item : cart){
+            itemDTOs.add(item.getItemDTO());
+        }
+        return itemDTOs;
+    }
+
+    /**
+     * Search for an already existing item in cart
+     * 
+     * @param scannedItemID Represents the item id the cashier recently scanned
+     * @return return the item <code>position</code> in <code>cart</code> if found, 
+     *         if not found -1 is returned.
+     */
+    public int findInCart(int scannedItemID){
+        int position = -1;
+
+        for(int i = 0; i < cart.size(); i++){
+            int existingItemID = cart.get(i).getItemID();
+
+            if(scannedItemID == existingItemID)
+            {
+                position = i;
+                return position;
+            }
+        }
+
+        return position;
+    }
+}
